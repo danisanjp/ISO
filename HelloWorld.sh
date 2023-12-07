022d42f9d7d16f77b20906ddc13a6e601e473607
Daniel Sánchez <146337705+danisanjp@users.noreply.github.com>
2023-12-07 20:30:08 +0100

    Update HelloWorld.sh

--- HelloWorld.sh
+++ HelloWorld.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
 
-# Solicitar un valor por consola
-echo "Introduce tu nombre..."
-read nombre
+#Que es la linea anterior?
+#Dar permisos de ejecución con chmod +x update-hello.sh
+#Ejecutar desde la consola con ./update-hello.sh
 
+# Solicitar un valor por consola
+read -p "Cual es tu nombre?" nombre
 # Mostrar el saludo con el valor ingresado
 echo "Hola $nombre"
+
+
+
