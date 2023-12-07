0044c4df11e0986e34e161cc277d8d41e7f3bd0e
Daniel Sánchez <146337705+danisanjp@users.noreply.github.com>
2023-12-07 20:45:01 +0100

    Create Calculadora.sh

---
+++ Calculadora.sh
@@ -0,0 +1,15 @@
+
+#!/bin/bash
+
+read -p "Introduce un numero entero" valor1
+
+read -p "Introduce otro numero  entero" valor2
+
+suma=$((valor1+valor2))
+
+echo "La suma de $valor1 y $valor2 es igual a:" $suma
+
+#realiza el resto de operaciones
+
+
+
