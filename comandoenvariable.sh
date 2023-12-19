218031e527a8b32d48b0d70149cef9d7359f8539
Daniel Sánchez <146337705+danisanjp@users.noreply.github.com>
2023-12-19 18:57:27 +0100

    Update comandoenvariable.sh

--- comandoenvariable.sh
+++ comandoenvariable.sh
@@ -2,3 +2,7 @@
 archivos=$(find . -maxdepth 1 -type f | wc -l)
 directorios=$(find . -maxdepth 1 -type d | wc -l)
 echo "Hay $archivos archivos y $directorios directorios"
+echo ""
+echo ""
+listado=$(ls)
+echo $listado
